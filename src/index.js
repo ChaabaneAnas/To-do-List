8ff7bf1801bdbe116b68cc2d2495fa3ddbd62504
ChaabaneAnas <anas.chaabane98@gmail.com>
2022-07-14 00:22:04 +0100

    tdList structred

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-import "./style.css"
+import './style.css';
+
+const Tasks = [
+  { description: 'join second program session', completed: false, index: 5 },
+  { description: 'take a cold shower', completed: true, index: 1 },
+  { description: 'have breakfeast', completed: false, index: 2 },
+  { description: 'join ur morning session call', completed: false, index: 3 },
+  { description: 'have lunch ', completed: false, index: 4 },
+];
+
+const sortTasks = Tasks.slice(0);
+sortTasks.sort((a, b) => a.index - b.index);
+
+const populate = () => {
+  sortTasks.forEach((task) => {
+    const ListWrapper = document.querySelector('.tdList');
+    const ListElement = document.createElement('li');
+    ListElement.textContent = `${task.description}`;
+    ListWrapper.appendChild(ListElement);
+  });
+};
+document.addEventListener('DOMContentLoaded', populate());
